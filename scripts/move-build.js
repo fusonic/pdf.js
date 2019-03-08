f1036e9d8c9c2add2360f571766de6a58542825a
Clemens Himmer <clemens.himmer@fusonic.net>
2019-03-08 11:29:00 +0100

    add build script & "docs"

---
+++ scripts/move-build.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const del = require('del');
+const path = require('path');
+
+const resourcePath = path.join(__dirname)
+
+del.sync([
+    resourcePath + '/build/minified/**/*.map',
+    resourcePath + '/build/minified/**/*.pdf'
+]);
+
+fs.copySync(
+    path.join(resourcePath, 'build', 'minified'),
+    path.join(resourcePath, 'dist'),
+);
+
+fs.remove(resourcePath, () => process.exit(code));
